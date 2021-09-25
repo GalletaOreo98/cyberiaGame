c1a9ccb5ffe73bc9ca6497c193f774e8d48a7fae
Christopher <82610384+GalletaOreo98@users.noreply.github.com>
2021-09-24 18:37:03 -0600

    Agregado de objetos

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -10,7 +10,7 @@ $(function () {
         $('#nickWrap').hide();
         $('#contentWrap').show();
         myName = $nickName.val();
-        socket.emit('new user', {nombre:myName,w:60,h:63,x:100,y:100}, data =>{
+        socket.emit('new user', {nombre:myName,w:60,h:63,x:100,y:634}, data =>{
             for (let index = 0; index < usersCliente.length; index++) {
                 if (data[index].nombre === usersCliente[index].nombre) {
                     usersCliente[index] = data[index];
@@ -47,9 +47,24 @@ var imgPersonaje, imgFondo;
 function cargarImagenes() {
     imgPersonaje = new Image();
     imgFondo = new Image();
+    imgMaquinaExpendedora= new Image();
+    imgMesa = new Image();
+    imgParlante = new Image();
+    imgRotuloBebidas = new Image();
+    imgRotuloCyberia = new Image();
+    imgRotuloWifi = new Image();
+    imgSilla= new Image();
 
     imgPersonaje.src = '/data/images/LainF1.png';
     imgFondo.src = '/data//images/fondo.jpg';
+    imgMaquinaExpendedora.src = '/data//images/maquinaExpendedora.png';
+    imgMesa.src = '/data//images/mesa.png';
+    imgParlante.src = '/data//images/parlante.png';
+    imgRotuloBebidas.src = '/data//images/rotuloBebidas.png';
+    imgRotuloCyberia.src = '/data//images/rotuloCyberia.png';
+    imgRotuloWifi.src = '/data//images/rotuloWifi.png';
+    imgSilla.src = '/data//images/silla.png';
+    
 }
 
 function inicializar() {
@@ -77,6 +92,17 @@ function dibujarFondo() {
     ctx.drawImage(imgFondo, 0, 0, 800, 600, 0, 0, 800, 600);
 }
 
+function dibujarObjetos() {
+    ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 634, 117, 120);
+    ctx.drawImage(imgMesa, 0, 0, 89, 45, 5, 634, 89, 45);
+    ctx.drawImage(imgParlante, 0, 0, 109, 40, 200, 634, 109, 45);
+    ctx.drawImage(imgRotuloBebidas, 0, 0, 79, 58, 660, 380, 79, 58);
+    /*ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 634, 117, 120);
+    ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 634, 117, 120);
+    ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 634, 117, 120);
+    ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 634, 117, 120);
+    ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 634, 117, 120);*/
+}
 
 //Principal bucle
 var fps = 55;
@@ -88,5 +114,6 @@ setInterval(function () {
 function principal() {
     borrarCanvas();
     dibujarFondo(); 
+    //dibujarObjetos();
     dibujarPersonajes();
 }
