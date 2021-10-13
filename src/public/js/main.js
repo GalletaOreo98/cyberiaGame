c82f1cc32c739353b80dfab0e40dac2e7c198fa1
Christopher <82610384+GalletaOreo98@users.noreply.github.com>
2021-10-13 17:36:35 -0600

    Nuevo login Y mejoras rendimiento

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,6 +5,7 @@ $(function () {
     const $nickName = $('#nickName');
     const $nickError = $('#nickError');
     var myName;
+    console.log('No deberías estar aquí ¿Sabes?');
 
     $nickForm.submit(e => {
         e.preventDefault();
@@ -71,7 +72,7 @@ function cargarImagenes() {
     imgPersonajeRF2 = new Image();
     imgPersonajeIF1 = new Image();
     imgPersonajeIF2 = new Image();
-    imgFondo = new Image();
+    /*imgFondo = new Image();
     imgMaquinaExpendedora = new Image();
     imgMesa = new Image();
     imgParlante = new Image();
@@ -79,21 +80,25 @@ function cargarImagenes() {
     imgRotuloCyberia = new Image();
     imgRotuloWifi = new Image();
     imgSilla= new Image();
-    imgCalabaza= new Image();
+    imgCalabaza= new Image();*/
+    imgFondo = new Image();
+    imgTv= new Image();
 
-    imgMaquinaExpendedora.src = '/data/images/maquinaExpendedora.png';
+    //imgMaquinaExpendedora.src = '/data/images/maquinaExpendedora.png';
     imgPersonajeRF1.src = '/data/images/LainDerechaF1.png';
     imgPersonajeRF2.src = '/data/images/LainDerechaF2.png';
     imgPersonajeIF1.src = '/data/images/LainIzquierdaF1.png';
     imgPersonajeIF2.src = '/data/images/LainIzquierdaF2.png';
-    imgFondo.src = '/data/images/fondo.jpg';
+    /*imgFondo.src = '/data/images/fondo.jpg';
     imgMesa.src = '/data/images/mesa.png';
     imgParlante.src = '/data/images/parlante.png';
     imgRotuloBebidas.src = '/data/images/rotuloBebidas.png';
     imgRotuloCyberia.src = '/data/images/rotuloCyberia.png';
     imgRotuloWifi.src = '/data/images/rotuloWifi.png';
     imgSilla.src = '/data/images/silla.png';
-    imgCalabaza.src = '/data/images/calabaza.png'
+    imgCalabaza.src = '/data/images/calabaza.png';*/
+    imgTv.src = '/data/images/tv.png';
+    imgFondo.src = '/data/images/fondoTotal.png';
     
 }
 
@@ -138,14 +143,18 @@ function dibujarFondo() {
 }
 
 function dibujarObjetos() {
-    ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 414, 117, 120);
-    ctx.drawImage(imgCalabaza, 0, 0, 40, 34, 10, 534-78, 40, 34);
+    /*ctx.drawImage(imgMaquinaExpendedora, 0, 0, 117, 120, 600, 414, 117, 120);
+    ctx.drawImage(imgCalabaza, 0, 0, 40, 34, 10, 534-78, 40, 34); 
     ctx.drawImage(imgMesa, 0, 0, 89, 45, 5, 534-45, 89, 45);
     ctx.drawImage(imgParlante, 0, 0, 109, 40, 200, 534-45, 109, 45);
     ctx.drawImage(imgRotuloBebidas, 0, 0, 79, 58, 666, 310, 79, 58);
     ctx.drawImage(imgRotuloCyberia, 0, 0, 295, 131, 110, 50, 295, 131);
     ctx.drawImage(imgRotuloWifi, 0, 0, 27, 35, 50, 380, 27, 35);
-    ctx.drawImage(imgSilla, 0, 0, 38, 37, 105, 534-38, 38, 37);
+    ctx.drawImage(imgSilla, 0, 0, 38, 37, 105, 534-38, 38, 37);*/
+    ctx.fillStyle="white";
+    ctx.font="bold 15px Courier New"; 
+    ctx.fillText('Lets all love Lain', 200, 250);
+    ctx.drawImage(imgTv, 0, 0, 448, 99, 110, 200, 448, 99);
     
 }
 
